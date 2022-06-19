ebe17d5911abf11b1508e33b2638ad14e03dd5a4
Israel Oluwafemi Alabi <israelalabi97@yahoo.com>
2022-06-19 07:27:19 +0100

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,10 @@ export default function Main(){
     return(
         <main className="main">
             <section className="section1">
+            <div className="div-img-bg-mobi">
+                 <img src="./images/bg-intro-mobile.svg" className="img-bg"/>
+                 <img src="./images/image-mockups.png" className="mockups"/>
+            </div>
               <div className="intro">
                 <h1 className="next-gen"> Next generation digital banking</h1>
                 <p className="para">Take your financial life online. Your Easybank account will be a one-stop-shop 
@@ -25,7 +29,7 @@ export default function Main(){
                     </p>
                 </div>
                 <div className="reasons-list">
-                    <div>
+                    <div className="reasons-item">
                         <img src="./images/icon-online.svg" className=""/>
                         <h3>Online Banking</h3>
                         <p>
@@ -34,7 +38,7 @@ export default function Main(){
                         </p>
                     </div>
 
-                    <div>
+                    <div className="reasons-item">
                         <img src="./images/icon-budgeting.svg" className=""/>
                         <h3>Simple Budgeting</h3>
                         <p>
@@ -42,7 +46,7 @@ export default function Main(){
                             close to hitting your limits.
                         </p>
                     </div>
-                    <div>
+                    <div className="reasons-item">
                         <img src="./images/icon-onboarding.svg" className=""/>
                         <h3>Fast Onboarding</h3>
                         <p>
@@ -50,7 +54,7 @@ export default function Main(){
                             of your finances right away.
                         </p>
                     </div>
-                    <div>
+                    <div className="reasons-item">
                         <img src="./images/icon-api.svg" className=""/>
                         <h3> Open API</h3>
                         <p>
@@ -61,14 +65,14 @@ export default function Main(){
                 </div>
             </section>
             <section className="section3">
-                <div className="reasons">
-                   <h1 className="why-choose">Latest Articles</h1>
+                <div className="latest">
+                   <h1 className="latest-articles-text">Latest Articles</h1>
                 </div>
                 <div className="latest-articles-contents">
                     <div className="articles">
                         <img src="./images/image-currency.jpg" className="img-currency"/>
                         <h5>By Claire Robinson</h5>
-                        <h3>Receive money in any currency with no fees</h3>
+                        <a href="#"><h3 className="articles-topic">Receive money in any currency with no fees</h3></a>
                         <p>
                             The world is getting smaller and we’re becoming more mobile. So why should you be 
                             forced to only receive money in a single …
@@ -77,7 +81,7 @@ export default function Main(){
                     <div className="articles">
                         <img src="./images/image-restaurant.jpg" className="img-currency"/>
                         <h5>By Wilson Hutton</h5>
-                        <h3> Treat yourself without worrying about money</h3>
+                        <a href="#"><h3 className="articles-topic"> Treat yourself without worrying about money</h3></a>
                         <p>
                             Our simple budgeting feature allows you to separate out your spending and set 
                             realistic limits each month. That means you …
@@ -86,7 +90,7 @@ export default function Main(){
                     <div className="articles">
                         <img src="./images/image-plane.jpg" className="img-currency"/>
                         <h5>By Wilson Hutton</h5>
-                        <h3>Take your Easybank card wherever you go</h3>
+                        <a href="#"><h3 className="articles-topic">Take your Easybank card wherever you go</h3></a>
                         <p>
                             We want you to enjoy your travels. This is why we don’t charge any fees on purchases 
                             while you’re abroad. We’ll even show you …              
@@ -95,7 +99,7 @@ export default function Main(){
                     <div className="articles">
                         <img src="./images/image-confetti.jpg" className="img-currency"/>
                         <h5>By Claire Robinson</h5>
-                        <h3>Our invite-only Beta accounts are now live!</h3>
+                        <a href="#"><h3 className="articles-topic">Our invite-only Beta accounts are now live!</h3></a>
                         <p>
                             After a lot of hard work by the whole team, we’re excited to launch our closed beta. 
                             It’s easy to request an invite through the site ...
