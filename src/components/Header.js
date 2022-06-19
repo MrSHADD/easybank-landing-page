84382320cdc35f8842fa64082b91c5e2ee23b5ca
Israel Oluwafemi Alabi <israelalabi97@yahoo.com>
2022-06-19 07:25:38 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,31 +3,36 @@ import React from "react";
 export default function Header(){
     return(
         <header className="header">
-                    <nav className="navbar">
+                    
                         <img src="./images/logo.svg" className="img-logo"/>
-                        
+                    <div className="navbar">
                         <ul className="nav-menu">
                             <li className="nav-item">
                                 <a href="#" className="nav-link">Home</a>
+                                <span className="bar"></span>
                             </li>
                             <li className="nav-item">
                                 <a href="#" className="nav-link">About</a>
+                                <span className="bar"></span>
                             </li>
                             <li className="nav-item">
                                 <a href="#" className="nav-link">Contact</a>
+                                <span className="bar"></span>
                             </li>
                             <li className="nav-item">
                                 <a href="#" className="nav-link">Blog</a>
+                                <span className="bar"></span>
                             </li>
                             <li className="nav-item">
                                 <a href="#" className="nav-link">Careers</a>
+                                <span className="bar"></span>
                             </li>
                         </ul>
                         <button className="btn-req">Request Invite</button>
-                        <div class="hamburger">
+                    </div>
+                    <div class="hamburger">
                                 <img src="./images/icon-hamburger.svg"/>
-                        </div>
-                    </nav>
+                    </div>
         </header>
     )
 }
